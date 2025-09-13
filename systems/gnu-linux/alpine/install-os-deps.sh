706cd704eea8766768f8829b20754c88bf376288
jacksonporter <jackson@jacksonporterjp.com>
2025-09-13 17:08:59 -0600

    update

--- systems/gnu-linux/alpine/install-os-deps.sh
+++ systems/gnu-linux/alpine/install-os-deps.sh
@@ -3,6 +3,8 @@
 set -e
 
 run_command_as_root() {
+    echo "Running command as root: $@"
+
     if [ "$(id -u)" -eq 0 ]; then
         # Already root, run directly
         "$@"
@@ -15,14 +17,30 @@ run_command_as_root() {
 main() {
     printf "Installing OS-level dependencies for Alpine...\n"
 
+    printf "Updating package lists...\n"
     run_command_as_root apk update
+
+    printf "Upgrading packages...\n"
     run_command_as_root apk upgrade
+
+    printf "Installing new packages...\n"
     run_command_as_root apk add --no-cache \
         bash \
         zsh \
         curl \
         git \
-        build-base
+        build-base \
+        zlib-dev \
+        zlib \
+        yaml \
+        yaml-dev \
+        bzip2-dev \
+        xz-dev \
+        libffi-dev \
+        ncurses-dev \
+        readline-dev \
+        openssl-dev \
+        sqlite-dev
 }
 
 main "$@"
